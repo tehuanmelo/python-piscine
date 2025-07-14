75238ee4ab86567501baa7cc2d0a35fd118eb410
tehuanmelo <tehuanmelo@gmail.com>
2025-07-14 18:44:16 +0400

    ex04 done

---
+++ py-00/ex04/whatis.py
@@ -0,0 +1,16 @@
+import sys as s 
+
+if len(s.argv) == 1:
+    pass
+elif len(s.argv) == 2:
+    try:
+        num = int(s.argv[1])
+        abs_num = abs(num)
+        if abs_num % 2 == 0:
+            print("I'm Even.")
+        else:
+            print("I'm Odd.")
+    except ValueError:
+        print("AssertionError: argument is not an integer")     
+else:
+    print("AssertionError: more than one argument is provided")
